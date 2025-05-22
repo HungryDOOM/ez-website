13d7fa7f0da1c32048d5665b44c01fcb4705c7ed
HungryDOOM <brianjoelam12_@hotmail.com>
2025-05-21 22:28:07 -0600

    add del movie

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MovieList />
+
+  <main className='main'>
+    <div className="container min-vh-100">
+      <header className="d-flex align-items-center">
+        <h1 className="text-white">Catalogo de Peliculas</h1>
       </header>
+      
+      <main className="text-center">
+        <p className="text-white">Bienvenid&#64; al catalogo de peliculas MovieMax. Podras encontrar una selección de los mejores títulos de distintos generos como Accion, Aventura, Comedia, Terror, entre otros... para disfrutar solo, en compañia o en familia.</p>
+      </main>
+        <MovieList />
+
     </div>
+    <footer className="bg-dark text-white text-left py-3 fixed-bottom">
+      <div className="px-5">
+        <p>Nombre de Alumno: Brian Joel Alvarez Manriquez</p>
+        <p>Codigo de alumno: 222959239</p>
+        <p>Correo: brianjoelam12_&#64;hotmail.com</p>
+        <p>Materia: Conceptualización de entornos de desarrollo de aplicaciones y servicios</p>
+      </div>
+    </footer>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  
+  </main>
   );
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,33 +7,63 @@ const MovieList = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       const result = await axios.get('https://ez-website-bkn-production.up.railway.app/api/movies');
-      console.log(result.data);
       setMovies(result.data);
     };
     fetchMovies();
   }, []);
 
+
+const [formData, setFormData] = useState({ nombre: '', correo: '' });
+const handleChange = (e) => {
+  setFormData({ ...formData, [e.target.name]: e.target.value });
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await axios.post('https://ez-website-bkn-production.up.railway.app/api/movies', formData);
+    window.alert("Pelicula agregada");
+    window.location.reload();
+    console.log('Pelicula agregada:', response.data);
+  } catch (error) {
+    console.error('Error al agregar pelicula:', error);
+  }
+};
+
+const delMovie = async (id) => {
+  try {
+    await axios.delete(`https://ez-website-bkn-production.up.railway.app/api/movies/${id}`);
+    window.alert("Pelicula eliminada");
+    window.location.reload();
+  } catch (error) {
+    console.error('Error al eliminar película:', error);
+  }
+};
+
   return (
     <div className="container">
-      <form>
-        <label for="title">Titulo</label>
-        <input name='title' id='title' type='text'></input><br/>
-        <label for="genre">Genero</label>
-        <input name='genre' id='genre' type='text'></input><br/>
-        <label for="year">Año</label>
-        <input name='year' id='year' type='text'></input><br/>
-        <label for="director">Director</label>
-        <input name='director' id='director' type='text'></input><br/>
-        <button type='submit'>Agregar pelicula</button>
-      </form>
+      <div className='text-white d-flex justify-content-center align-items-center'>
+        <form className="p-4 bg-dark rounded" onSubmit={handleSubmit}>
+          <label for="title">Titulo</label>
+          <input className='form-control' name='title' id='title' type='text' onChange={handleChange}></input>
+          <label for="genre">Genero</label>
+          <input className='form-control' name='genre' id='genre' type='text' onChange={handleChange}></input>
+          <label for="year">Año</label>
+          <input className='form-control' name='year' id='year' type='number' onChange={handleChange}></input>
+          <label for="director">Director</label>
+          <input className='form-control' name='director' id='director' type='text' onChange={handleChange}></input><br/>
+          <button type='submit'>Agregar pelicula</button>
+        </form>
+      </div>
       <div className="row">
         <table className="table table-dark">
           <thead>
-            <tr className="h2">
+            <tr className="h2 text-center">
               <th>Titulo</th>
               <th>Genero</th>
               <th>Año</th>
               <th>Director</th>
+              <th>Accion</th>
             </tr>
           </thead>
           <tbody>
@@ -43,6 +73,7 @@ const MovieList = () => {
                 <td>{movie.genre}</td>
                 <td>{movie.year}</td>
                 <td>{movie.director}</td>
+                <td><button onClick={() => delMovie(movie._id)} className='bg-light border-0 btn btn-info rounded-circle'>-</button></td>
               </tr>
             ))}
           </tbody>
